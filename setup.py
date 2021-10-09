fcee418949fcdd00639948bd4048017302abbc1f
Tetsuro KITAJIMA <tetsuro@amanogawa.to>
2021-10-09 15:36:55 +0900

    Update setup.py
    ref: https://qiita.com/aidy91614/items/445776b14ddff78e79c1

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 """Package setup file."""
 import os
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 from setuptools import find_packages, setup
 
 
